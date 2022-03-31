cbd3799d841a716403ad871a27f5ea3a14583325
Daniella <idaniellazambrano@gmail.com>
2022-03-31 19:20:21 -0400

    Add station_3

---
+++ station_3.cpp
@@ -0,0 +1,84 @@
+/**
+ * @file station_3.cpp
+ * @brief This code is the Station 3 of the problem. 
+ * @version 0.1
+ * @date 2022-03-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <random>
+#include <chrono>
+#include <thread>
+#include <fstream>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include "json.hpp"
+#include "production_card.hpp"
+#include "utilities.hpp"
+
+using json = nlohmann::json;
+using namespace std::chrono_literals;
+
+int main(){
+    // Read parameters file
+    std::cout << "[ESTACION 3] Creando estación 3\n";
+    std::ifstream i("params.json");
+    json config;
+    i >> config;
+
+    // Create queue for CADENA_2
+    std::cout << "[ESTACION 3] Creando cadena de traslado entre estaciones 2 y 3\n";
+    std::string queue_name_2 = config["queues"]["cadena_2"];
+    int msgid_2 = create_msg_queue(queue_name_2);
+
+
+    // Create queue for CADENA_3
+    std::cout << "[ESTACION 3] Creando cadena de traslado entre estaciones 3 y 4\n";
+    std::string queue_name_3 = config["queues"]["cadena_3"];
+    int msgid_3 = create_msg_queue(queue_name_3);
+
+
+    std::normal_distribution<double> norm = get_normal_dist_object(config["station_3"]["mean_3"], config["station_3"]["deviation_3"]);
+
+
+    // POP cars from the queue
+    std::cout << "[ESTACION 3] Valor de media M3: " << config["station_3"]["mean_3"] << std::endl;
+    std::cout << "[ESTACION 3] Valor de desviacion estandard D3: " << config["station_3"]["deviation_3"] << std::endl;
+
+    ProductionCard pcard; 
+
+    while (true)
+    {
+        size_t data = msgrcv(msgid_2, &pcard, sizeof(pcard), 0, 0);
+        if (data == 0) {
+            std::cout << "[ESTACION 3] No hay vehículos en cola. " << std::endl;
+            std::this_thread::sleep_for(200ms);
+            continue;
+        }
+        else {
+            int seed = std::chrono::system_clock::now().time_since_epoch().count();
+            std::default_random_engine generator (seed);
+            double number = norm(generator);
+            std::chrono::duration<double> period ( number );
+
+            std::cout << "[ESTACION 3] Automovil "<< pcard.car_id <<" colocando motor y cableado ..." << std::endl;
+            std::cout << "[ESTACION 3] Tiempo estimado " << period.count() << std::endl;
+
+            std::this_thread::sleep_for( period );
+
+            // Add car bodywork
+            pcard.motor_type = (MotorType)(seed%3);
+            std::cout << "[ESTACION 3] El motor del automovil "<< pcard.car_bodywork <<" es: " << MOTOR_TYPE_STR[pcard.motor_type] << std::endl;
+
+            
+            std::cout << "[ESTACION 3] Enviando automóvil " << pcard.car_id << " a la siguiente estación..." << std::endl;
+            msgsnd(msgid_3,&pcard,sizeof(pcard),0);            
+        }
+    }
+
+    return 0;
+}
