f38f9862875395c326a30b0b4612318ea6675c30
Jaf4r <jaf4rdev@gmail.com>
2022-03-31 14:23:10 -0400

    Add station 2

--- Makefile
+++ Makefile
@@ -2,11 +2,13 @@ CC = g++
 VERSION = -std=c++14
 LIBS = -lpthread
 
+all: station_1 station_2
+
 station_1: station_1.cpp json.hpp
 	$(CC) $(VERSION) -I. station_1.cpp -o station_1.exe $(LIBS)
 
 station_2: station_2.cpp json.hpp
-	$(CC) $(VERSION) -I. station_2.cpp -o station_2.exe $(LIBS)
+	$(CC) $(VERSION) -I. station_2.cpp -o station_2.exe
 
 tests: test/test_json.cpp test/test_production_card.cpp
 	$(CC) $(VERSION) -I. test/test_json.cpp -o test_json.exe
--- station_2.cpp
+++ station_2.cpp
@@ -25,7 +25,7 @@ using namespace std::chrono_literals;
 
 int main(){
     // Read parameters file
-    std::cout << "[ESTACION 2] Creando estación 1\n";
+    std::cout << "[ESTACION 2] Creando estación 2\n";
     std::ifstream i("params.json");
     json config;
     i >> config;
@@ -34,17 +34,23 @@ int main(){
     std::cout << "[ESTACION 2] Creando cadena de traslado entre estaciones 1 y 2\n";
     std::string queue_name_1 = config["queues"]["cadena_1"];
     int msgid_1 = create_msg_queue(queue_name_1);
-    std::cout<<"Key: "<<msgid_1;
 
 
+    // Create queue for CADENA_2
+    std::cout << "[ESTACION 2] Creando cadena de traslado entre estaciones 2 y 3\n";
+    std::string queue_name_2 = config["queues"]["cadena_2"];
+    int msgid_2 = create_msg_queue(queue_name_2);
 
 
-
-    std::normal_distribution<double> norm = get_normal_dist_object(
-        config["station_1"]["mean_1"], config["station_1"]["deviation_1"]
-    );
+    std::normal_distribution<double> norm = get_normal_dist_object(config["station_2"]["mean_2"], config["station_2"]["deviation_2"]);
 
 
+    // POP cars from the queue
+    std::cout << "[ESTACION 2] Valor de media M2: " << config["station_2"]["mean_2"] << std::endl;
+    std::cout << "[ESTACION 2] Valor de desviacion estandard D2: " << config["station_2"]["deviation_2"] << std::endl;
+
+
+    CarColor color_counter = CarColor::red;
     ProductionCard pcard; 
 
 
@@ -53,21 +59,33 @@ int main(){
         size_t data = msgrcv(msgid_1, &pcard, sizeof(pcard), 0, 0);
         if (data == 0) {
             std::cout << "[ESTACION 2] No hay vehículos en cola. " << std::endl;
-            std::this_thread::sleep_for(500ms);
+            std::this_thread::sleep_for(200ms);
             continue;
         }
         else {
             int seed = std::chrono::system_clock::now().time_since_epoch().count();
             std::default_random_engine generator (seed);
-            std::cout << "[ESTACION 2] Automovil entrando a pintura" << std::endl;
             double number = norm(generator);
             std::chrono::duration<double> period ( number );
-            // std::cout << "[ESTACION 1] Procesando chasis y asignando identificador al automóvil. Tiempo estimado " << period.count() << std::endl;
+
+            std::cout << "[ESTACION 2] Automovil "<< pcard.car_id <<" entrando a pintura" << std::endl;
+            std::cout << "[ESTACION 2] Tiempo estimado " << period.count() << std::endl;
+
+            // Add color to the car
+            color_counter = (CarColor)(((int)(color_counter)+1)%3);
+            pcard.color = color_counter;
+            std::cout << "[ESTACION 2] Color asignado al automovil "<< pcard.car_id <<":" <<CAR_COLORS_STR[pcard.color] << std::endl;
+
 
             std::this_thread::sleep_for( period );
+
+            // Add car bodywork
+            pcard.car_bodywork = (CarBodywork)(seed%2);
+            std::cout << "[ESTACION 2] Carroceria asignada al automovil "<< pcard.car_id <<":" <<CAR_BODYWORK_STR[pcard.car_bodywork] << std::endl;
+
             
             std::cout << "[ESTACION 2] Enviando automóvil " << pcard.car_id << " a la siguiente estación..." << std::endl;
-            // msgsnd(msgid_1,&pcard,sizeof(pcard),0);            
+            msgsnd(msgid_2,&pcard,sizeof(pcard),0);            
         }
     }
 
